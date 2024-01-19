0e98f23f2afff707af6d79be5cd591556c7b7296
MeenaAlagiah <117711147+MeenaAlagiah@users.noreply.github.com>
2024-01-19 15:36:17 +0530

    Committing the Vue Card Getting Started sample

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# getting-started-with-the-vue-card-component
-A quick start Vue project that shows how to add the Vue Card component to a Vue application. It shows how to add header, image and content to the card. It also shows how to display horizontal cards, add separators and action buttons to the card.
+# Getting Started with the Vue Card Component
+A quick start Vue project that shows how to add the Vue Card component to a Vue application. It shows how to add header, image and content to the card. It also shows how to display horizontal cards, add separators and action buttons to the card. 
+ 
+Refer to the following documentation to learn about the Vue Card component: 
+https://ej2.syncfusion.com/vue/documentation/card/vue3-getting-started
+
+Check out this online example of the Vue Card Component:
+https://ej2.syncfusion.com/vue/demos/#/material3/card/basic.html
+
+Make sure that you have the latest versions of NodeJS and Visual Studio Code in your machine before starting to work on this project.
+
+### How to run this application?
+
+To run this application, you need to clone the `getting-started-with-the-vue-card-component` repository and then open it in Visual Studio Code. Now, simply install all the necessary vue packages into your current project using the `npm install` command and run your project using the `npm run dev` command.
---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="container">
+    <div class="e-card e-card-horizontal">
+        <div class="e-card-image"></div>  
+        <div class="e-card-stacked"> 
+        <div class="e-card-header">
+            <div class="e-card-header-image profile"></div>
+            <div class="e-card-header-caption">
+                <div class="e-card-title">Laura Chauhan</div>
+                <div class="e-card-sub-title">Sales Coordinator</div>
+            </div>
+            </div>
+        <div class="e-card-separator"></div>
+        <div class="e-card-content">
+            Laura Chauhan received a BA in psychology from the University of Washington.
+            She has also completed a course in Business French. She reads and writes French.  
+        </div>
+        <div class="e-card-actions e-card-vertical">
+            <button class="e-card-btn">Like</button>
+            <button class="e-card-btn">Subscribe</button>
+        </div>
+       </div>  
+    </div>
+
+</div>
+</template>
+
+<style scoped>
+@import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-vue-layouts/styles/material.css';
+.container
+{
+    margin:50px auto 0; 
+    width:500px; 
+    height:700px;
+}
+.e-card-header-image.profile
+{
+      background-image: url('../src/assets/profile.jpg');
+    height: 60px !important;
+    width: 60px !important;
+}
+.e-card-image
+{
+    background-image: url('../src/assets/sales.jpg');
+    
+}
+</style>
